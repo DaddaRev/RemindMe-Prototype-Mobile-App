4f811edb85dfab873ce7f49bc76122722198580e
DaddaRev <davireve@gmail.com>
2025-12-10 14:28:30 +0100

    Client first draft upload

---
+++ client/remind-me/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="android-device">
+      <div className="screen-content">
+        {/* Status Bar */}
+        <div className="status-bar">
+          <div className="status-left">
+            <span className="time">9:41</span>
+          </div>
+          <div className="status-right">
+            <div className="signal">
+              <div className="signal-bar"></div>
+              <div className="signal-bar"></div>
+              <div className="signal-bar"></div>
+            </div>
+            <span className="wifi">📡</span>
+            <div className="battery">
+              <div className="battery-level"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* App Content Area */}
+        <div className="app-container"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
